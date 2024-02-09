4d2bb39689de76fbf08d6e1541914560ca09b447
SASwagoto <saswagoto@gmail.com>
2024-02-10 03:10:03 +0600

    ok

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
    <section class="w-full mt-4">
-      <div class="container mx-auto flex justify-between">
+      <div class="container mx-auto flex justify-center gap-2">
          <div class="w-2/4">
             <div class="headnews border rounded-md flex gap-2 p-2">
                <div class="w-3/5 overflow-hidden rounded-md">
@@ -14,7 +14,18 @@
                </div>
             </div>
          </div>
-         <div class="w-1/4"></div>
+         <div class="w-1/4">
+            <div class="flex flex-col gap-2">
+               <div class="rounded-md flex flex-col gap-1 p-2 border">
+                  <div class="rounded-md overflow-hidden">
+                     <img class="w-full" src="{{asset('frontend/images/news1.webp')}}" alt="">
+                  </div>
+                  <div class="mt-1">
+                     <h3 class="text-xl font-bold">'শীতলক্ষ্যা ধ্বংসে পরিবেশ অধিদপ্তরও জড়িত'</h3>
+                  </div>
+               </div>
+            </div>
+         </div>
          <div class="w-1/4"></div>
       </div>
       
